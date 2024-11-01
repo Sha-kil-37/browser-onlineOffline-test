cd4cd4aeb0783817c2ff9ce0c1f79c2ea709d685
Shakil <sakildevmern@gmail.com>
2024-11-01 16:23:57 +0600

    last commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  window.location.href = "http://127.0.0.1:5500/test.html";
+}
+// check onLine
+function checkOnline() {
+  alert("broser now online");
+}
+// check offline
+function checkOffline() {
+  alert("browser now offline");
+}
